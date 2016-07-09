a57730b9332d369a51c92907e4d14f871bd3c145
Allan Ing <xeero@yahoo.com>
2016-07-09 00:34:00 -0500

    Parse results

--- PriceCheck.py
+++ PriceCheck.py
@@ -1,28 +1,41 @@
 from bs4 import BeautifulSoup
 import urllib2
+import re
 
 divider = "====================================================================================="
 
-print "Price check...\n"
+print("Price check...\n")
 
 if True:
-    print divider
-    print 'Merrell Moab Ventilator Wide'
+    print(divider)
+    print('Merrell Moab Ventilator Wide (Target $80)')
     url = "http://www.merrell.com/US/en/moab-ventilator-wide-width/16220M.html"
     content = urllib2.urlopen(url).read()
     soup = BeautifulSoup(content)
-    print "Sale: ", soup.find_all("span", class_="promo-price")
-    print soup.find_all("span", itemprop="price")
-    print
+    price = soup.find_all("span", class_="promo-price")
+    match = re.search("\d+\.\d\d", str(price))
+    if match != None:
+        print("  SALE: $%s" % match.group(0))
+    else:
+        price = soup.find_all("span", itemprop="price")
+        match = re.search("\d+\.\d\d", str(price))
+        if match != None:
+            print("  Regular Price: $%s" % match.group(0))
+        else:
+            print("  Error retrieving price.")
 
 if True:
-    print divider
-    print 'Nebraska Furniture Mart Polk 12" sub'
+    print(divider)
+    print('Nebraska Furniture Mart Polk 12" sub (Target $270)')
     url = "https://www.nfm.com/DetailsPage.aspx?productid=31539166"
     content = urllib2.urlopen(url).read()
     soup = BeautifulSoup(content)
-    print soup.find_all("div", class_="yourprice")
-    print
+    price = soup.find_all("div", class_="yourprice")
+    match = re.search("\d+\.\d\d", str(price))
+    if match != None:
+        print("  $%s" % match.group(0))
+    else:
+        print("  Error retrieving price.")
 
-print divider
+print(divider)
 
